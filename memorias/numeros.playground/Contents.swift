a25fa798901edaf39df73b58a79b073e35a8ff07
José Luis Pérez Cruces <luispc@macbook-pro-de-jose.local>
2017-04-19 00:28:43 -0500

    segunda sección

---
+++ memorias/numeros.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+func calcularNumeroDivisible(numero : Int, divisible : Int)->Bool{
+    var resultado : Bool = false
+    
+    if (numero % divisible == 0){
+        resultado = true
+    }
+    
+    return resultado
+}
+
+var numeros = 0...100
+
+for numero in numeros{
+    // Calcula los números divisibles entre 5
+    if (calcularNumeroDivisible(numero, divisible: 5)){
+        print("# \(numero) Bingo!!!")
+    }
+    
+    //Calcula los números divisibles entre par o impar
+    if (calcularNumeroDivisible(numero, divisible: 2)){
+        print("# \(numero) par!!!")
+    }else{
+        print("# \(numero) impar!!!")
+    }
+    
+    
+    if (numero >= 30 && numero <= 40){
+        print("# \(numero) viva Swift!!!")
+    }
+    
+    
+    
+}
